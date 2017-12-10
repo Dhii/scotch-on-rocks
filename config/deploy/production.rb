cee3be8fe29d2ec271f28cd7e6a00dccdf55b30c
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-10 16:58:43 +0100

    Will now ask for password during SSH authentication

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,16 +8,17 @@ set :stage, :production
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
+ask(:password, nil)
+server 'example.com', user: 'deploy', roles: %w{web app db}, password: fetch(:password)
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
+  set :ssh_options, {
 #    keys: %w(~/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+    forward_agent: false,
+    auth_methods: %w(password)
+  }
 
 fetch(:default_env).merge!(wp_env: :production)
